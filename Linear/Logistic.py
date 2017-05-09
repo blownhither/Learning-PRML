1ee711b8cacd9d1b8edb4ac6edfb9681deb1cfe9
bh <mzy_martin@hotmail.com>
2017-05-10 00:56:42 +0800

    Debugging Logistics

--- Linear/Logistic.py
+++ Linear/Logistic.py
@@ -9,14 +9,13 @@ class LogisticRegressionClassifier():
         self.w = None
         self.y = None
         self.x = None
-        self.last_cost = -np.inf
+        self.last_cost = 0
+        self.reg_ratio = None
 
     def cost(self, reg=True):
         wx = np.dot(self.x, self.w)
-
         wx = np.clip(wx, -500, 500)
-
-        ret1 = - np.sum(wx * self.y)
+        ret1 = -np.sum(wx * self.y)
         ret2 = np.sum(np.log(1 + np.exp(wx)))
 
         if reg is True:
@@ -24,15 +23,15 @@ class LogisticRegressionClassifier():
             ret1 += norm(self.w)
         return ret1 + ret2
 
-    def gradient(self, reg=True):
+    def gradient(self):
         wx = np.dot(self.x, self.w)
 
         wx = np.clip(wx, -500, 500)
 
         exp = np.exp(wx)
-        ret = np.dot(self.y - exp / (exp + 1.0), self.x)
-        if reg is True:
-            ret += 2 * self.w
+        ret = - np.dot(self.y - exp / (exp + 1.0), self.x)
+
+        ret += self.w / self.reg_ratio
 
         if np.isnan(ret).any():
             raise Exception('Stop here!')
@@ -52,19 +51,21 @@ class LogisticRegressionClassifier():
         self.last_cost = cost
         return ret
 
-    def train(self, x, y, reg_ratio):
+    def train(self, x, y, reg_ratio=20):
         # use row vector
         x = np.array(x)
         self.n_samples, self.n_features = x.shape
         self.x = np.concatenate((x, np.ones((self.n_samples, 1))), 1)
         self.w = np.random.rand(self.n_features + 1)
 
+        self.reg_ratio = reg_ratio
+
         self.y = np.array(y)
 
         assert len(y) == self.n_samples
 
-        for i in range(10000):
-            improvement = self._gradient_decrease(0.1)
+        for i in range(1000):
+            improvement = self._gradient_decrease(0.8)
 
     def predict(self, x):
         x = np.array(x)
