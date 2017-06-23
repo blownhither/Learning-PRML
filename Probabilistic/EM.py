bebd03cc3a9d678a13282feb633587b4dd5237f8
bh <mzy_martin@hotmail.com>
2017-06-24 01:33:10 +0800

    Coding EM, seems wrong, need test

--- Probabilistic/EM.py
+++ Probabilistic/EM.py
@@ -12,6 +12,7 @@ class GMM:
         self.a = a / a.sum()
         self.mu = np.random.rand(k)
         self.sigma = np.random.rand(k)
+        self.sigma2 = np.square(self.sigma)
         self._cum = np.cumsum(self.a)
 
     def _choose(self, n):
@@ -33,12 +34,62 @@ class GMM:
 class EM(GMM):
     def __init__(self, k, data):
         """
-        EM is a GMM model undergoing iterative updating
+        Here EM is a GMM model undergoing iterative updating
         :param k:
         :param data:
         """
         super().__init__(k)
+        self.data = data
+        self.n = len(data)
 
+    def _gaussian_density(self, single_data):
+        """
+        :param single_data: should be ONE single data
+        :return:
+        """
+        return self.sigma * np.exp(- np.square(single_data - self.mu) / 2.0 / self.sigma2) / np.sqrt(np.pi * 2) / self.sigma
+
+    def expectation(self):
+        """
+        :return: response[data_j, model_k]
+        """
+        response = np.zeros((self.n, self.k))
+        for i in range(self.n):
+            res = self.a * self._gaussian_density(self.data[i])
+            response[i] = res / res.sum()
+        return response
+
+    def maximization(self, response):
+        r_sum = np.sum(response, 0)
+        mu = np.dot(response.T, self.data) / r_sum
+        sigma2 = np.zeros(self.k)
+        for i in range(self.k):
+            sigma2[i] = np.dot(response[:, i], np.square(self.data - self.mu[i]))
+        sigma2 /= r_sum
+        a = r_sum / self.n
+
+        self.mu = mu
+        self.sigma2 = sigma2
+        self.sigma = np.sqrt(sigma2)
+        self.a = a
+
+    def fit(self, n_iter=100):
+        for i in range(n_iter):
+            self.maximization(self.expectation())
+
+    def test(self, mu, sigma, a):
+        err_mu = np.linalg.norm(self.mu - mu)
+        err_sigma = np.linalg.norm(self.sigma - sigma)
+        err_a = np.linalg.norm(self.a - a)
+        print(err_mu, err_sigma, err_a)
+
+    def fit_and_test(self, n_iter, mu, sigma, a):
+        for i in range(n_iter):
+            self.maximization(self.expectation())
+            err_mu = np.linalg.norm(self.mu - mu)
+            err_sigma = np.linalg.norm(self.sigma - sigma)
+            err_a = np.linalg.norm(self.a - a)
+            print(err_mu, err_sigma, err_a)
 
 
 def test_GMM():
@@ -47,5 +98,12 @@ def test_GMM():
     prec = np.mean(o) - np.dot(g.a, g.mu)
     print(prec)
 
+
+def test_EM():
+    g = GMM(2)
+    data = g.observe(20000)
+    e = EM(2, data)
+    e.fit_and_test(10000, g.mu, g.sigma, g.a)
+
 if __name__ == '__main__':
-    test_GMM()
+    test_EM()
