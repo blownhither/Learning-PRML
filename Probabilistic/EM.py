8338e5b6dc5cb7ee5d3e132fb4067bdea133004e
bh <mzy_martin@hotmail.com>
2017-06-27 01:12:16 +0800

    Coded dim, need test

--- Probabilistic/EM.py
+++ Probabilistic/EM.py
@@ -1,30 +1,43 @@
 import numpy as np
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
+
+
+def rand_positive(n):
+    """
+    Generate positive semi-definite matrix with size (n, n)
+    :param n: mat size
+    :return: np.array
+    """
+    ret = np.random.random((n, n))
+    return np.dot(ret.T, ret)
 
 
 class GMM:
-    def __init__(self, k, a=None, mu=None, sigma=None):
+    def __init__(self, k=2, dim=2, a=None, mu=None, sigma=None):
         """
         A GMM model generating data with distribution of $\sum{a_k \phi(y|\theta_k)}$
         :param k:
         """
         self.k = k
+        self.dim = dim
         if a is None:
-            a = np.random.rand(k)
+            a = np.random.random(k)
         else:
             a = np.array(a)
         self.a = a / a.sum()
         self._cum = np.cumsum(self.a)
 
         if mu is None:
-            self.mu = np.random.rand(k)
+            self.mu = np.random.random((dim, k))
         else:
             self.mu = np.array(mu)
+            assert self.mu.shape == (dim, k)
 
         if sigma is None:
-            self.sigma = np.random.rand(k)
+            self.sigma = np.dstack([rand_positive(dim) for _ in range(k)])
         else:
             self.sigma = np.array(sigma)
+            assert self.sigma.shape == (dim, dim, k)
         self.sigma2 = np.square(self.sigma)
 
     def _choose(self, n):
@@ -37,29 +50,34 @@ class GMM:
     def observe(self, n):
         """
         Get n randomly generated observations from GMM model
-        :return: np.array, shape=(n,)
+        :return: np.array, shape=(n, dim)
         """
         index = self._choose(n)
-        return np.random.normal(self.mu[index], self.sigma[index])
+        ret = np.zeros((n, self.dim))
+        for i in index:
+            ret[i] = np.random.multivariate_normal(self.mu[:, i], self.sigma[:, :, i])
+        return ret
 
-    def observe_and_plot(self, n):
-        index = self._choose(n)
-        data = np.random.normal(self.mu[index], self.sigma[index])
-        plt.hist(data[index == 0], alpha=0.6)
-        plt.hist(data[index == 1], alpha=0.6)
-        plt.show()
-        return data
+    # def observe_and_plot(self, n):
+    #     index = self._choose(n)
+    #     data = np.random.normal(self.mu[index], self.sigma[index])
+    #     plt.hist(data[index == 0], alpha=0.6)
+    #     plt.hist(data[index == 1], alpha=0.6)
+    #     plt.show()
+    #     return data
 
 
 class EM(GMM):
-    def __init__(self, k, data):
+    def __init__(self, k, dim, data):
         """
         Here EM is a GMM model undergoing iterative updating
         :param k:
         :param data:
         """
-        super().__init__(k)
+        super().__init__(k, dim)
         self.data = data
+        assert data.shape[1] == dim
+
         self.n = len(data)
 
     def _gaussian_density(self, single_data):
@@ -67,7 +85,15 @@ class EM(GMM):
         :param single_data: should be ONE single data
         :return:
         """
-        return np.exp(- np.square(single_data - self.mu) / 2.0 / self.sigma2) / np.sqrt(np.pi * 2) / self.sigma
+        # return np.exp(- np.square(single_data - self.mu) / 2.0 / self.sigma2) / np.sqrt(np.pi * 2) / self.sigma
+        ret = np.zeros(self.k)
+        for i in range(self.k):
+            temp1 = np.power(np.linalg.det(2 * np.pi * self.sigma[:, :, i]), -0.5)
+            temp2 = np.mat(single_data - self.mu[:, i])         # as row
+            temp3 = np.exp(-0.5 * temp2 * np.linalg.pinv(self.sigma[:, :, i]) * temp2.T)
+            ret[i] = temp1 * temp3
+        return ret
+
 
     def expectation(self):
         """
@@ -82,9 +108,10 @@ class EM(GMM):
     def maximization(self, response):
         r_sum = np.sum(response, 0)
         mu = np.dot(response.T, self.data) / r_sum
-        sigma2 = np.zeros(self.k)
+        sigma2 = np.zeros((self.dim, self.dim, self.k))
         for i in range(self.k):
-            sigma2[i] = np.dot(response[:, i], np.square(self.data - self.mu[i]))
+            sigma2[i] = np.dot(response[:, i], np.square(self.data - mu[:, i]))
+            # sigma2[i] = np.dot(response[:, i], np.square(self.data - self.mu[:, i]))
         sigma2 /= r_sum
         a = r_sum / self.n
 
@@ -106,10 +133,11 @@ class EM(GMM):
     def fit_and_test(self, n_iter, mu, sigma, a):
         for i in range(n_iter):
             self.maximization(self.expectation())
-            err_mu = np.linalg.norm(self.mu - mu)
-            err_sigma = np.linalg.norm(self.sigma - sigma)
-            err_a = np.linalg.norm(self.a - a)
-            print(err_mu, err_sigma, err_a)
+            if i % 10 == 0:
+                err_mu = np.linalg.norm(self.mu - mu)
+                err_sigma = np.linalg.norm(self.sigma - sigma)
+                err_a = np.linalg.norm(self.a - a)
+                print(i, ":", err_mu, err_sigma, err_a)
 
 
 def test_gmm():
@@ -120,10 +148,11 @@ def test_gmm():
 
 
 def test_em():
-    g = GMM(2, [0.5, 0.5], [-1, 1], [1, 1])
+    e = np.eye(2, 2)
+    g = GMM(2, 2)
     # g.observe_and_plot(1000)
-    data = g.observe(10000)
-    e = EM(2, data)
+    data = g.observe(20000)
+    e = EM(2, 2, data)
     e.fit_and_test(10000, g.mu, g.sigma, g.a)
 
 if __name__ == '__main__':
