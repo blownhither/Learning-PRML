70464a8641233cf8d8af1abf4c43a38e89dbe93d
bh <mzy_martin@hotmail.com>
2017-06-30 21:41:50 +0800

    Need debugging Gibbs

--- Probabilistic/Gibbs.py
+++ Probabilistic/Gibbs.py
@@ -1,6 +1,7 @@
 import numpy as np
 from math import gamma
 
+from GMM import GMM
 
 class Gibbs:
     def __init__(self, k, dim, data):
@@ -10,36 +11,128 @@ class Gibbs:
         assert data.shape[1] == dim
         self.n = len(data)
 
-        self.a = 1          # as a_k            # TODO: setting
-        self.a0 = self.a * k
-        self.k0 = 0.1                           # TODO: ?
+        self.a = 1.0                # TODO: setting
+        self.a0 = self.a
+        self.ak = self.a0 / self.k
+        self.k0 = 0.1  # TODO: ?
         self.kn = self.k0 + self.n
         self.v0 = 1
-        self.m0 = np.zeros(dim)                 # TODO: check
-        self.mn = (self.k0 * self.m0 + self.n * np.mean(data, 0)) / self.kn
+        # self.vn = self.v0 + self.n
+        self.m0 = np.zeros(dim)     # TODO: check
+        # self.mn = (self.k0 * self.m0 + self.n * np.mean(data, 0)) / self.kn
         self.s0 = np.zeros((dim, dim))
-        s = np.sum(np.square(data), 1)
-        self.sn = self.s0 + s + self.k0 * np.square(self.m0).sum() - self.kn * np.square(self.mn).sum()
+        # s = self._cov(data)
+        # self.sn = self.s0 + s + self.k0 * self.n / (self.k0 + self.n) * self._cov(
+        #     (np.mean(data, 0) - self.m0).reshape((1, -1)))
+        # self.sn_det = np.power(np.linalg.det(self.sn), -self.vn / 2.0)
 
         self.z = np.random.randint(0, k, size=self.n)
         self.freq = np.bincount(self.z)
-        self.inv_freq = self.n - self.freq
+        # self.inv_freq = self.n - self.freq
+
+    def _cov(self, data):
+        ret = np.zeros((self.k, self.k))
+        for d in data:
+            ret += np.dot(d.reshape((self.dim, 1)), d.reshape((1, self.dim)))
+        return ret
+
+    def _cov_single(self, data):
+        return np.dot(data.reshape((self.dim, 1)), data.reshape((1, self.dim)))
 
     def _term1(self, omit):
         """
         term1 in Gibbs sampling is $P(z_i=k | z_{\i}, \alpha)$
         """
-        self.inv_freq[self.z[omit]] -= 1
-        ret = (self.inv_freq + self.a) / (self.n + self.a0 - 1)
-        self.inv_freq[self.z[omit]] += 1
+        self.freq[self.z[omit]] -= 1
+        ret = (self.freq + self.ak) / (self.n + self.a - 1)
+        self.freq[self.z[omit]] += 1
         return ret
 
-    def _student(self, mu, sigma, freedom):
-        p = sigma.shape[0]
-        t1 = gamma((freedom + p) / 2.0)
-        t2 = gamma(freedom / 2.0) *
+        # def _student(self, mu, sigma, freedom):
+        # p = sigma.shape[0]
+        # t1 = gamma((freedom + p) / 2.0)
+        # t2 = gamma(freedom / 2.0) * np.power(freedom * np.pi, p / 2.0) * np.sqrt(np.linalg.det(sigma))
+        # center =
+        # t3 = np.power(1 + )
 
-    def _term2(self, omit):
+    def _term2(self, k, x_star):
         """
         term2 in Gibbs sampling is $p(x_i | x_{k\i}, \beta)$
         """
+        index = np.where(self.z == k)
+        data = self.data[index]
+        n = len(index)
+        kn = self.k0 + n
+        vn = self.v0 + n
+        sn = self.s0 + self._cov(data) + self._cov_single(data.mean(0)) * self.k0 * n / (self.k0 + n)
+        sn_star = sn + self._cov_single(x_star)
+
+        serial = list(map(gamma, np.arange((vn + 1 - self.dim) / 2.0, (vn + 1.1) / 2.0, 0.5)))
+        assert len(serial) == self.dim + 1
+
+        prod = np.prod(serial)
+        numerator1 = np.power(np.pi, -self.dim / 2.0) * prod / serial[0]
+        denominator1 = prod / serial[-1]
+        numerator2 = np.power(np.linalg.det(sn_star), -(vn + 1) / 2.0)
+        denominator2 = np.power(np.linalg.det(sn), -vn / 2.0)
+        return numerator1 * numerator2 / denominator1 / denominator2
+
+    def fit(self, max_iter):
+        for _iter in range(max_iter):
+            for i in range(self.n):
+                t1 = self._term1(i)
+                t2 = np.zeros(self.k)
+                self.freq[self.z[i]] -= 1
+                self.z[i] = -1                                  # take it out
+                for k in range(self.k):
+                    t2[k] = self._term2(k, self.data[i])        # TODO: not right
+                cum = np.cumsum(t1 * t2)
+                cum /= cum[-1]
+                k_new = np.where(np.random.random() < cum)[0][0]
+                self.z[i] = k_new
+                self.freq[k_new] += 1
+
+                assert self.n == self.freq.sum()
+
+    def test(self, mu_t, sigma_t, pi_t):
+        mu = []
+        sigma = []
+        pi = []
+        for i in range(self.k):
+            c = self.data[self.z == i]
+            pi.append(len(c) / float(self.n))
+            mu.append(c.mean(0))
+            sigma.append(c.std(0))
+        err_mu = np.linalg.norm(mu_t - np.array(mu))
+        err_sigma = np.linalg.norm(sigma_t - np.array(sigma))
+        err_pi = np.linalg.norm(pi_t - np.array(pi))
+        print(err_mu, err_sigma, err_pi)
+        return err_mu, err_sigma, err_pi
+
+    def fit_and_test(self, max_iter, mu_t, sigma_t, pi_t):
+        for _iter in range(max_iter):
+            if _iter % 5 == 0:
+                print(_iter)
+                self.test(mu_t, sigma_t, pi_t)
+
+            for i in range(self.n):
+                t1 = self._term1(i)
+                t2 = np.zeros(self.k)
+                for k in range(self.k):
+                    t2[k] = self._term2(k, self.data[i])       # TODO: not right
+                cum = np.cumsum(t1 * t2)
+                cum /= cum[-1]
+                k_new = np.where(np.random.random() < cum)[0][0]
+                self.z[i] = k_new
+
+
+def test():
+    g = GMM(2, 2)
+    data = g.observe(500)
+    gb = Gibbs(2, 2, data)
+    gb.fit_and_test(1000, g.mu, g.sigma, g.a)
+
+if __name__ == '__main__':
+    test()
+
+
