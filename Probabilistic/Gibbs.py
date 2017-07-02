7a3bd379259d085e5792e6ec8a98f810c5d2565f
bh <mzy_martin@hotmail.com>
2017-07-02 17:44:07 +0800

    Clean up codes and add doc

--- Probabilistic/Gibbs.py
+++ Probabilistic/Gibbs.py
@@ -2,8 +2,10 @@ import numpy as np
 from math import gamma
 from scipy.special import gammaln
 from scipy.stats import multivariate_normal, wishart
+import pandas as pds
 
 from Probabilistic.GMM import GMM, rand_positive
+from Probabilistic.EM import *
 
 
 class Gibbs:
@@ -14,16 +16,17 @@ class Gibbs:
         assert data.shape[1] == dim
         self.n = len(data)
 
-        self.a = 1.0                # TODO: setting
+        self.a = 1.0
         self.a0 = self.a
         self.ak = self.a0 / self.k
-        self.k0 = 10.0               # TODO: ?
+        self.k0 = 10.0
         self.kn = self.k0 + self.n
-        self.v0 = dim + 10             # (v0 > dim + 1)must holds
-        # self.m0 = np.ones(dim)     # TODO: check
-        self.m0 = np.sum(data, 0)
+        self.v0 = dim + 5             # (v0 > dim + 1)must holds
+        # self.m0 = np.ones(dim)
+        # self.s0 = np.ones((dim, dim))
+        self.m0 = np.mean(data, 0)
         self.s0 = np.cov(data.T)
-
+        # self.s0 = np.zeros((dim, dim))
         self.z = np.random.randint(0, k, size=self.n)
         self.freq = np.bincount(self.z)
 
@@ -57,23 +60,28 @@ class Gibbs:
         kn = self.k0 + n
         vn = self.v0 + n
 
-        data_cov = self._cov(data)
+        # data_cov = self._cov(data)
+        data_cov = np.cov(data.T)
         data_mean = np.mean(data, 0)
-        data_mean_star = (np.mean(data, 0) * n + x_star) / (n + 1)
-        mn = (self.k0 * self.m0 + n * data_mean) / kn
-        mn_star = (self.k0 * self.m0 + (n + 1) * data_mean_star) / kn
-        sn = self.s0 + data_cov + self.k0 * self._cov_single(self.m0) + kn * self._cov_single(mn)
-        sn_star = self.s0 + data_cov + self._cov_single(x_star) + self.k0 * self._cov_single(self.m0) + (kn + 1) * self._cov_single(mn_star)
 
-        numerator1 = np.power(np.pi * (kn + 1) / kn, -self.dim / 2.0)
-        temp = np.linalg.det(sn_star)
-        numerator2 = np.power(temp / np.linalg.det(sn), -vn / 2.0) * np.power(temp, -0.5)
-        numerator3 = np.exp(gammaln((vn + 1) / 2.0) - gammaln((vn + 1 - self.dim) / 2.0))
-        ret = numerator1 * numerator2 * numerator3
+        # TODO: this works well but it is not the original scheme in the paper
+        return multivariate_normal(data_mean, data_cov).pdf(x_star)
 
-        return ret
+        # TODO: the original scheme in the paper
+        # data_mean_star = (np.mean(data, 0) * n + x_star) / (n + 1)
+        # mn = (self.k0 * self.m0 + n * data_mean) / kn
+        # mn_star = (self.k0 * self.m0 + (n + 1) * data_mean_star) / kn
+        # sn = self.s0 + data_cov + self.k0 * self._cov_single(self.m0) + kn * self._cov_single(mn)
+        # sn_star = self.s0 + data_cov + self._cov_single(x_star) + self.k0 * self._cov_single(self.m0) + (kn + 1) * self._cov_single(mn_star)
+        # numerator1 = np.power(np.pi * (kn + 1) / kn, -self.dim / 2.0)
+        # temp = np.linalg.det(sn_star)
+        # numerator2 = np.power(temp / np.linalg.det(sn), -vn / 2.0) * np.power(temp, -0.5)
+        # numerator3 = np.exp(gammaln((vn + 1) / 2.0) - gammaln((vn + 1 - self.dim) / 2.0))
+        # ret = numerator1 * numerator2 * numerator3
+        # return ret
 
     def _form_dist(self):
+        """ From status sequence, rebuild k Gaussian distributions """
         mu = np.zeros((self.dim, self.k))
         sigma = np.zeros((self.dim, self.dim, self.k))
         pi = np.zeros(self.k)
@@ -85,6 +93,7 @@ class Gibbs:
         return mu, sigma, pi
 
     # def _form_dist(self):
+    #    """ Form k Gaussian distributions with sampling method """
     #     mu = np.zeros((self.dim, self.k))
     #     sigma = np.zeros((self.dim, self.dim, self.k))
     #     _pi = np.bincount(self.z) / float(self.n)
@@ -93,41 +102,19 @@ class Gibbs:
     #     for i in range(self.k):
     #         mu[:, i], sigma[:, :, i] = self._update_component(i)
     #     return mu, sigma, pi
-    #
-    # def _update_component(self, i):
-    #     index = self.z == i
-    #     data = self.data[index]
-    #     s = len(data)
-    #     if s == 0:
-    #         sigma = wishart.rvs(df=self.dim, scale=np.linalg.inv(self.s0))
-    #         mu = np.random.multivariate_normal(self.m0, sigma)
-    #     else:
-    #         data_sum = data.sum(0)
-    #         data_mean = data_sum / s
-    #
-    #         b_term = self._cov(data - data_mean)
-    #         m_dash = (self.k0 * self.m0 + data_sum) / (s + self.k0)
-    #         # c_dash = s + 0.1
-    #         a_dash = s + self.dim
-    #         b_dash = self.s0 + b_term + s / (self.dim * s + 1) * self._cov_single(data_mean - self.m0)
-    #
-    #         sigma = wishart.rvs(df=a_dash, scale=np.linalg.inv(b_dash))
-    #         if self.dim > 1:
-    #             mu = np.random.multivariate_normal(m_dash, sigma)
-    #         else:
-    #             mu = np.random.normal(m_dash, sigma)
-    #     return mu, sigma
 
     def test(self):
+        """ Routine API: print likelihood """
         mu, sigma, pi = self._form_dist()
         self.log_likelihood(mu, sigma, pi)
 
     def fit_and_test(self, max_iter):
+        """ Combine fit and test routine """
         for _iter in range(max_iter):
             if _iter % 1 == 0:
                 self.test()
-                # mu, sigma, pi = self._form_dist()
-                # print(self.log_likelihood(mu, sigma, pi))
+                mu, sigma, pi = self._form_dist()
+                print(self.log_likelihood(mu, sigma, pi))
             for i in range(self.n):
                 t1 = self._term1(i)
 
@@ -143,13 +130,11 @@ class Gibbs:
                 k_new = np.where(np.random.random() < cum)[0][0]
                 self.z[i] = k_new
                 self.freq[k_new] += 1
-            print(self.freq)
 
     def predict(self):
         return self._form_dist()
 
     def log_likelihood(self, mu, sigma, pi):
-        # mu, sigma, pi = self._form_dist()
         mat = np.zeros((self.n, self.k))
         for i in range(self.k):
             dist = multivariate_normal(mu[:, i], sigma[:, :, i])
@@ -157,19 +142,14 @@ class Gibbs:
                 mat[j, i] = dist.pdf(self.data[j])
         evaluation = np.sum(np.log(np.dot(mat, pi)))
         print(evaluation)
+        return evaluation
 
 
 def test():
-    g = GMM(3, 2)
+    g = GMM(5, 2)
     data = g.observe(1000)
-    # data.tofile('Probabilistic/tmp/gibbs/data.txt', sep=' ')
-    # plt = g.plot()
-    gb = Gibbs(3, 2, data)
-    # print('Target log-likelihood')
-    # gb.log_likelihood(g.mu, g.sigma, g.a)
-
+    gb = Gibbs(5, 2, data)
     gb.fit_and_test(100)
-    mu, sigma, a = gb.predict()
 
 
 if __name__ == '__main__':
