1c9dcb6ab629a6a586484ea610451d46d45c02d0
blownhither <blownhitherma@gmail.com>
2017-07-19 17:44:21 +0800

    Runnable Word2Vec

--- NeuralNetwork/Word2Vec.py
+++ NeuralNetwork/Word2Vec.py
@@ -17,6 +17,15 @@ import tensorflow as tf
 
 URL = "http://mattmahoney.net/dc/"
 VOCAB_SIZE = 10000
+BATCH_SIZE = 128
+EMBEDDING_SIZE = 128
+SKIP_WINDOW = 1
+N_SKIPS = 2
+
+VALID_SIZE = 16
+VALID_WINDOW = 100
+VALID_EXAMPLES = np.random.choice(VALID_WINDOW, VALID_SIZE, replace=False)
+N_SAMPLED = 64
 
 
 def maybe_download(filename, n_bytes):
@@ -85,7 +94,7 @@ class W2VBatchMaker:
             avoid = {skip_window}
             for j in range(num_skips):
                 while target in avoid:
-                    target = np.random.randint(0, span - 1)
+                    target = np.random.randint(0, span)
                 avoid.add(target)
                 batch[i + j] = buffer[skip_window]
                 labels[i + j] = buffer[target]
@@ -109,8 +118,71 @@ def run():
     print('Most common words ', count[:5])
     print('Sample data', data[:10], [reverse_dictionary[d] for d in data[:10]])
     batch_maker = W2VBatchMaker(data)
-    ans = batch_maker.get(8, 2, 1)
-    print(ans)
+
+    # define graph
+    graph = tf.Graph()
+    with graph.as_default():
+        train_inputs = tf.placeholder(tf.int32, shape=[BATCH_SIZE])
+        train_labels = tf.placeholder(tf.int32, shape=[BATCH_SIZE, 1])
+        valid_dataset = tf.constant(VALID_EXAMPLES, dtype=tf.int32)
+
+        with tf.device('/cpu:0'):
+            embeddings = tf.Variable(tf.random_uniform([VOCAB_SIZE, EMBEDDING_SIZE], -1, 1))
+            embedded = tf.nn.embedding_lookup(embeddings, train_inputs)
+            nce_weight = tf.Variable(
+                tf.truncated_normal([VOCAB_SIZE, EMBEDDING_SIZE], stddev=1.0 / np.sqrt(EMBEDDING_SIZE))
+            )
+            nce_biases = tf.Variable(tf.zeros([VOCAB_SIZE]))
+
+        loss = tf.reduce_mean(tf.nn.nce_loss(weights=nce_weight,
+                                             biases=nce_biases,
+                                             labels=train_labels,
+                                             inputs=embedded,
+                                             num_sampled=N_SAMPLED,
+                                             num_classes=VOCAB_SIZE))
+
+        # optimizer
+        optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+        norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
+        normalized_embeddings = embeddings / norm
+        valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings, valid_dataset)
+        similarity = tf.matmul(valid_embeddings, normalized_embeddings, transpose_b=True)
+        init = tf.global_variables_initializer()
+
+        # start training
+        N_STEPS = 100000
+        with tf.Session(graph=graph) as session:
+            init.run()
+            print("Initialized")
+
+            average_loss = 0
+            for step in range(N_STEPS):
+                batch_inputs, batch_labels = batch_maker.get(BATCH_SIZE, N_SKIPS, SKIP_WINDOW)
+                feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
+                _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
+                average_loss += loss_val
+
+                if step % 2000 == 0:
+                    print("step %d, loss %g" % (step, average_loss / 2000))
+                    average_loss = 0
+
+                if step % 10000 == 0:
+                    sim = similarity.eval()
+                    for i in range(VALID_SIZE):
+                        valid_word = reverse_dictionary[VALID_EXAMPLES[i]]
+                        top_k = 2
+                        nearest = (-sim[i, :]).argsort()[1:top_k + 1]
+                        log_str = "Nearset to %s:" % valid_word
+                        for k in range(top_k):
+                            close_word = reverse_dictionary[nearest[k]]
+                            log_str += " " + close_word
+                        print(log_str)
+
+            final_embeddings = normalized_embeddings.eval()
+            print('Final embeddings %g' % final_embeddings)
+
+
+
 
 if __name__ == '__main__':
     run()
