a05a4b3fd5b680deb61e76c43731906338b19b65
blownhither <blownhitherma@gmail.com>
2017-07-19 10:54:41 +0800

    Add reindex

--- Util/func_tools.py
+++ Util/func_tools.py
@@ -1,7 +1,16 @@
 import numpy as np
+from collections import Counter
 
 
 def accuracy(x, y):
     x = np.array(x)
     y = np.array(y)
     return np.count_nonzero(x == y) / len(x)
+
+
+def reindex(x):
+    x = np.array(x)
+    ret = np.zeros(x.shape)
+    for idx, key in enumerate(set(x)):
+        ret[x == key] = idx
+    return ret
