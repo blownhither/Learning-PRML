f1c76d005e12a6447e30983f6ce5094419e42925
bh <mzy_martin@hotmail.com>
2017-07-31 22:16:14 +0800

    Implemented LVQ

--- Clustering/GeneralClustering.py
+++ Clustering/GeneralClustering.py
@@ -11,12 +11,51 @@ import numpy as np
 
 class GeneralClustering:
     def __init__(self, x, y=None):
+        """
+        General type for Clustering classes. Y is None for unsupervised clustering
+        """
         x = np.array(x)
         self.n_data = x.shape[0]
         self.dim = x.shape[1]
         self._scale = np.mean(x, 0), np.std(x, 0)
         self.x = (x - self._scale[0]) / self._scale[1]    # data normalized
-        if y is None:
+        if y is not None:
             self.y = y
             self.types = set(y)
             self.n_types = len(self.types)
+
+    @staticmethod
+    def _external_index(x, y, y_):
+        # s->same, d->different for each pair of samples
+        # e.g. ss stands for pair with same label both in y and y_
+        ss, sd, ds, dd = 0, 0, 0, 0
+
+        x = np.array(x)
+        n = len(x)
+        for i in range(n):                  # for each pair
+            for j in range(i + 1, n):
+                if y[i] == y[j]:
+                    if y_[i] == y_[j]:      # same-same
+                        ss += 1
+                    else:                   # same-diff
+                        sd += 1
+                else:
+                    if y_[i] == y_[j]:      # diff-same
+                        ds += 1
+                    else:                   # diff-diff
+                        ds += 1
+        return ss, sd, ds, dd
+
+    @staticmethod
+    def jaccard_coefficient(x, y, y_):
+        """
+        return Jaccard Coefficient on arguments $\frac{SS}{SS + SD + DS}$
+        :param x: Data
+        :param y: Predicted label
+        :param y_: Truth label
+        :return: Jaccard Coefficient on arguments
+        """
+        ss, sd, ds, _ = GeneralClustering._external_index(x, y, y_)
+        if ss + sd + ds == 0:
+            return np.inf
+        return float(ss) / (ss + sd + ds)
--- Clustering/LVQ.py
+++ Clustering/LVQ.py
@@ -18,9 +18,9 @@ class LVQ(GeneralClustering):
         super(LVQ, self).__init__(x, y)
         self.centers = None         # center of each cluster (prototype vector)
 
-    def fit(self, learning_rate):
+    def fit(self, learning_rate, max_iter=1000):
         self.centers = np.random.random((self.n_types, self.dim))
-        while True:
+        for iter_ in range(max_iter):
             choice = np.random.randint(self.n_data)
             x_, y_ = self.x[choice], self.y[choice]             # random sample
             dist = np.linalg.norm(x_ - self.centers, 2, 1)      # distance to each prototype vectors
@@ -31,7 +31,35 @@ class LVQ(GeneralClustering):
                 update = (1 + learning_rate) * self.centers[idx] - learning_rate * x_
             self.centers[idx] = update
 
+            if iter_ % 50 == 0:
+                n_sample = max(int(self.n_data * 0.05), 1)
+                idx = np.random.randint(0, self.n_data, n_sample)
+                samples = self.x[idx]                           # 5% samples
+                prediction = self.predict(samples)
+                jc = self.jaccard_coefficient(samples, prediction, self.y[idx])
+                print('Iter %d, Jaccard Coefficient %g' % (iter_, jc))
+
+    def _predict_one(self, x):
+        return np.argmin(np.linalg.norm(x - self.centers, 2, 1))    # nearest prototype vector
+
+    def predict(self, x):
+        x_ = np.array(x)
+        if x_.ndim == 1:
+            return self._predict_one(x)
+        elif x_.ndim == 2:
+            return np.array([self._predict_one(d) for d in x_])
+        else:
+            raise TypeError('predict(x) takes row-wise matrix x or single-row')
+
+
+def _test():
+    import pandas as pds
+    data = pds.read_csv('../Dataset/watermelon-tiny.csv')
+    x = np.array(data[data.columns[1:-1]]).repeat(20, axis=0)
+    y = np.array(data[data.columns[-1]]).repeat(20)
+    lvq = LVQ(x, y)
+    lvq.fit(1e-2, 100000)
 
 
 if __name__ == '__main__':
-    pass
+    _test()
