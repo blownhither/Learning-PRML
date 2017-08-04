44186e0668f5eb62081f72260ee71692651ae2e7
blownhither <blownhitherma@gmail.com>
2017-08-04 13:19:46 +0800

    Finish AGNES

--- Clustering/AGNES.py
+++ Clustering/AGNES.py
@@ -18,9 +18,44 @@ class AGNES(GeneralClustering):
 
     def fit(self, k):
         """
-        Fit a clustering forest that contains k trees
+        Fit with a clustering forest until it contains k clusters
+        Using max pair distance as distance between two clusters
         :param k: Final number of clusters
-        :return:
+        :return: labels assigned to each sample
         """
-        
+        dist = np.zeros((self.size, self.size))
+        labels = np.arange(self.size)
+        for i in range(self.size):
+            dist[i, i] = np.inf
+            for j in range(i + 1, self.size):
+                temp = np.linalg.norm(self.x[i] - self.x[j])
+                dist[i, j] = temp
+                dist[j, i] = temp
+        for _ in range(self.size, k, -1):
+            i, j = np.unravel_index(dist.argmin(), dist.shape)  # nearest cluster pair
+            labels[labels == j] = i                             # combine cluster i and j
+            temp = np.maximum(dist[i, :], dist[j, :])           # re-calculate dist mat
+            dist[i, :] = temp
+            dist[:, i] = temp
+            dist[j, :] = np.inf                                 # cluster j disappear
+            dist[:, j] = np.inf
 
+        # rearrange labels
+        for i, v in enumerate(set(labels)):
+            if i != v:
+                labels[labels == v] = i
+        return labels
+
+
+def _test():
+    import pandas as pds
+    data = pds.read_csv('../Dataset/watermelon-numeric.csv')
+    a = AGNES(data)
+    labels = a.fit(7)
+    print(labels)
+    for i in range(7):
+        print(np.where(labels == i))
+
+
+if __name__ == '__main__':
+    _test()
